c4d97cfc35ca11f385709934f81d616f0fa1ad2f
jkhelil <jkhelil@redhat.com>
2020-10-20 09:44:06 +0200

    fix command in csv

---
+++ scripts/index.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker build --build-arg OPERATOR_VERSION_NEXT=${OPERATOR_VERSION} -f openshift-ci/index.Dockerfile -t ${OPERATOR_REPOSITORY}/${OPERATOR_INDEX_IMAGE}:${OPERATOR_INDEX_VERSION} .
